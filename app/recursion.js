abd74cce5b09b3ad541973787b9885a88d599881
Jeff Louie <jpl7690@yahoo.com>
2014-11-30 22:16:13 -0800

    file recursion working. working on doing file recursion with a subdirectory

--- app/recursion.js
+++ app/recursion.js
@@ -3,7 +3,25 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     listFiles: function(data, dirName) {
+      var _this = this;
+      var files = data.files;
+      var arr = [];
 
+      // if(dirName){
+      //   while(data.dir !== dirName){
+      //     data = files;
+      //     files = files.files;
+      //   }
+      // }
+      files.forEach(function(file){
+        if(typeof file !== 'object'){
+          arr.push(file);
+        }
+        else{
+          arr = arr.concat(_this.listFiles(file));
+        }
+      });
+      return arr;
     },
 
     permute: function(arr) {
